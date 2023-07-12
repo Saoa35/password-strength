b5f7eb41ec53f5243a4f84371c3c82b907af93dc
Saoa35 <saoamah@gmail.com>
2023-07-12 16:57:05 +0400

    cleaned html(return), added labelColor func. & openCloseSwitcher var.

--- src/components/PasswordForm.jsx
+++ src/components/PasswordForm.jsx
@@ -10,7 +10,6 @@ export const PasswordForm = () => {
 
   const handleCheckClick = (e) => {
     e.preventDefault();
-
     setShowPassword(!showPassword);
   };
 
@@ -19,6 +18,22 @@ export const PasswordForm = () => {
     setGreenField(false);
     setYellowField(false);
   };
+
+  const openCloseSwitcher = !showPassword ? (
+    <svg viewBox="0 0 60 54" xmlns="http://www.w3.org/2000/svg">
+      <g id="Layer_2" data-name="Layer 2">
+        <path d="m44 31h-2v-10a10 10 0 0 0 -20 0v10h-2a3 3 0 0 0 -3 3v16a3 3 0 0 0 3 3h24a3 3 0 0 0 3-3v-16a3 3 0 0 0 -3-3zm-20-10a8 8 0 0 1 16 0v10h-16zm21 29a1 1 0 0 1 -1 1h-24a1 1 0 0 1 -1-1v-16a1 1 0 0 1 1-1h24a1 1 0 0 1 1 1z" />
+        <path d="m32 37a5 5 0 1 0 5 5 5 5 0 0 0 -5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1 -3 3z" />
+      </g>
+    </svg>
+  ) : (
+    <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+      <g id="Layer_2" data-name="Layer 2">
+        <path d="m26 47a5 5 0 1 0 -5-5 5 5 0 0 0 5 5zm0-8a3 3 0 1 1 -3 3 3 3 0 0 1 3-3z" />
+        <path d="m44 11a10 10 0 0 0 -10 10v10h-20a3 3 0 0 0 -3 3v16a3 3 0 0 0 3 3h24a3 3 0 0 0 3-3v-16a3 3 0 0 0 -3-3h-2v-10a8 8 0 0 1 16 0v5h2v-5a10 10 0 0 0 -10-10zm-6 22a1 1 0 0 1 1 1v16a1 1 0 0 1 -1 1h-24a1 1 0 0 1 -1-1v-16a1 1 0 0 1 1-1z" />
+      </g>
+    </svg>
+  );
 
   const handlePaChange = (e) => {
     setPassword(e.target.value);
@@ -31,8 +46,6 @@ export const PasswordForm = () => {
     const easyParagraph = passwordCheck.querySelector(".easy");
     const mediumParagraph = passwordCheck.querySelector(".medium");
     const strongParagraph = passwordCheck.querySelector(".strong");
-
-    passwordCheck.className = "password_check";
 
     if (e.target.value === "") {
       easyParagraph.className = "easy gray";
@@ -78,32 +91,23 @@ export const PasswordForm = () => {
     }
   };
 
+  const labelColor = () => {
+    return redField
+      ? "label red-focus focused_red"
+      : yellowField
+      ? "label yellow-focus focused_yellow"
+      : greenField
+      ? "label green-focus focused_green"
+      : "label";
+  };
+
   return (
     <form className="form">
       <span className="title">Password:</span>
 
-      <label
-        htmlFor="password"
-        className={
-          redField
-            ? "label red-focus focused_red"
-            : yellowField
-            ? "label yellow-focus focused_yellow"
-            : greenField
-            ? "label green-focus focused_green"
-            : "label"
-        }
-      >
+      <label htmlFor="password" className={labelColor()}>
         <input
-          className={
-            redField
-              ? "input red-focus"
-              : yellowField
-              ? "input yellow-focus"
-              : greenField
-              ? "input green-focus"
-              : "input"
-          }
+          className="input"
           type={showPassword ? "text" : "password"}
           id="password"
           value={password}
@@ -112,33 +116,8 @@ export const PasswordForm = () => {
           placeholder="Enter password"
         />
 
-        <button
-          className={
-            redField
-              ? "check_btn red-focus"
-              : yellowField
-              ? "check_btn yellow-focus"
-              : greenField
-              ? "check_btn green-focus"
-              : "check_btn"
-          }
-          onClick={handleCheckClick}
-        >
-          {!showPassword ? (
-            <svg viewBox="0 0 60 54" xmlns="http://www.w3.org/2000/svg">
-              <g id="Layer_2" data-name="Layer 2">
-                <path d="m44 31h-2v-10a10 10 0 0 0 -20 0v10h-2a3 3 0 0 0 -3 3v16a3 3 0 0 0 3 3h24a3 3 0 0 0 3-3v-16a3 3 0 0 0 -3-3zm-20-10a8 8 0 0 1 16 0v10h-16zm21 29a1 1 0 0 1 -1 1h-24a1 1 0 0 1 -1-1v-16a1 1 0 0 1 1-1h24a1 1 0 0 1 1 1z" />
-                <path d="m32 37a5 5 0 1 0 5 5 5 5 0 0 0 -5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1 -3 3z" />
-              </g>
-            </svg>
-          ) : (
-            <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-              <g id="Layer_2" data-name="Layer 2">
-                <path d="m26 47a5 5 0 1 0 -5-5 5 5 0 0 0 5 5zm0-8a3 3 0 1 1 -3 3 3 3 0 0 1 3-3z" />
-                <path d="m44 11a10 10 0 0 0 -10 10v10h-20a3 3 0 0 0 -3 3v16a3 3 0 0 0 3 3h24a3 3 0 0 0 3-3v-16a3 3 0 0 0 -3-3h-2v-10a8 8 0 0 1 16 0v5h2v-5a10 10 0 0 0 -10-10zm-6 22a1 1 0 0 1 1 1v16a1 1 0 0 1 -1 1h-24a1 1 0 0 1 -1-1v-16a1 1 0 0 1 1-1z" />
-              </g>
-            </svg>
-          )}
+        <button className="check_btn" onClick={handleCheckClick}>
+          {openCloseSwitcher}
         </button>
       </label>
 
